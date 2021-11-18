8cb34a42c5511a2c4d335c58b93fdfba304b7f2f
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-18 15:18:30 +0100

    sysdata

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Polling station data
+#'
+#' Polling station data with IDs. Collected from \url{https://info.aws.dk/afstemningsomraader/}
+#'
+#' @format A `tibble` with 1,384 rows and 3 columns
+"vs"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,13 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+vs <- utils::read.csv("C:/Users/jevi/OneDrive - Epinion/Documents/GitHub/PollingStationsDK/Geografiske_stamdata.csv",
+                      sep = ";")
+
+vs <- vs %>%
+  dplyr::select(c(Valgsted.navn,
+                  Valgsted.Id,
+                  KommuneNr)) %>%
+  dplyr::rename(ps_id = Valgsted.Id) %>%
+  tibble()
+
+usethis::use_data(vs, overwrite = TRUE, internal = TRUE)
