74fc1f59627f6756d420fd5586fd325101c6aeb7
jvieroe <jvieroe@gmail.com>
2021-11-29 22:17:33 +0100

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' Polling station data
 #'
-#' Polling station data with IDs. Collected from \url{https://valgdatabase.dst.dk/?lang=en}
+#' Polling station data with IDs
 #'
 #' @format A `tibble` with 1,384 rows and 3 columns
+#' @source Collected from \url{https://valgdatabase.dst.dk/?lang=en}
 "vs"
