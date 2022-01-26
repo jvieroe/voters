fea55fe331c2ef1513ef3bc00f432384ddba6682
Jeppe Vierø <43220957+jvieroe@users.noreply.github.com>
2022-01-26 10:26:53 +0100

    edit workhorse

--- R/workhorse.R
+++ R/workhorse.R
@@ -3,8 +3,40 @@
 check_getPS <- function(spatial,
                         id) {
 
+  if(class(spatial) == "numeric") {
+    stop("Invalid 'spatial' argument provided. Must be logical")
+  }
+
+  if(class(spatial) == "character") {
+    stop("Invalid 'spatial' argument provided. Must be logical")
+  }
+
+  if(class(spatial) == "integer") {
+    stop("Invalid 'spatial' argument provided. Must be logical")
+  }
+
+  if(class(spatial) == "factor") {
+    stop("Invalid 'spatial' argument provided. Must be logical")
+  }
+
   if(!spatial %in% c(TRUE, FALSE)) {
     stop("Invalid 'spatial' argument provided. Must be logical")
+  }
+
+  if(class(id) == "numeric") {
+    stop("Invalid 'id' argument provided. Must be logical")
+  }
+
+  if(class(id) == "character") {
+    stop("Invalid 'id' argument provided. Must be logical")
+  }
+
+  if(class(id) == "integer") {
+    stop("Invalid 'id' argument provided. Must be logical")
+  }
+
+  if(class(id) == "factor") {
+    stop("Invalid 'id' argument provided. Must be logical")
   }
 
   if(!id %in% c(TRUE, FALSE)) {
